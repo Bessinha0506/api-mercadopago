780b482506c3e3a744dec26b3daf6add8969a415
Bessinha0506 <matheusbessa.pessoal@gmail.com>
2025-10-07 01:16:24 -0300

    app.py

--- app.py
+++ app.py
@@ -78,6 +78,3 @@ def webhook():
             print(f"ERRO ao processar webhook: {e}")
 
     return jsonify({"status": "ok"}), 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
